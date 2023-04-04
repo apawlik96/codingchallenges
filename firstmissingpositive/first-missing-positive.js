10b36316e184e8e1f2e7e4c8e8301a121cb444cf
Ania <aniaa.pawlik@gmail.com>
2023-04-04 13:03:53 +0200

    code prep

--- firstmissingpositive/first-missing-positive.js
+++ firstmissingpositive/first-missing-positive.js
@@ -1,19 +1,18 @@
 // https://leetcode.com/problems/first-missing-positive/
 
-function firstMissingPositive (nums) {
-    nums.sort((a,b) => a - b)
-    const array = nums.filter( numb => numb > 0)
-    if (array[0] !== 1){
+function firstMissingPositive(nums) {
+    nums.sort((a, b) => a - b)
+    const array = nums.filter(numb => numb > 0)
+    if (array[0] !== 1) {
         return 1
     }
-    for(let i=0; i<array.length-1; i++){
-        let add = array[i+1] - array[i]
-        if(add !== 1){
-        return array[i] + 1
-        } else if (add === 1) {
-        return array[array.length-1] + 1
+    for (let i = 0; i < array.length - 1; i++) {
+        let add = array[i + 1] - array[i]
+        if (add !== 1) {
+            return array[i] + 1
         }
+        return array[array.length - 1] + 1
     }
 }
 
-firstMissingPositive([3,4,1])
+firstMissingPositive([3, 4, 1])
