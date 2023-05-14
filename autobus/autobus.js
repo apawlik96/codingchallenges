b956b0cfc9d445c6d0ce3fc16fb582035a701aee
Ania <aniaa.pawlik@gmail.com>
2023-05-14 23:49:06 +0200

    code prep

--- autobus/autobus.js
+++ autobus/autobus.js
@@ -1,23 +1,24 @@
-// https://codeforces.com/problemset/problem/1679/a ZROBIONE
+// https://codeforces.com/problemset/problem/1679/a 
 function autobus(numberOfWheels){
     if (numberOfWheels % 4 === 0){
-        console.log("Liczba autobusów z 2 osiami: " + numberOfWheels / 4)
+        return "Liczba autobusów z 2 osiami: " + numberOfWheels / 4
     }
     if(numberOfWheels % 6 === 0) {
-        console.log("Liczba autobusów z 3 osiami: " + numberOfWheels / 6)
+        return "Liczba autobusów z 3 osiami: " + numberOfWheels / 6
     }
-    var sumOfWheels = Math.floor(numberOfWheels / 10) * 10
-    var remainder = numberOfWheels - sumOfWheels
-    var sumOfBus = sumOfWheels / 10
-    var numberWithTwoAxles = remainder / 4
-    var numberWithThreeAxles = remainder / 6
-    var sum = 0
+    const sumOfWheels = Math.floor(numberOfWheels / 10) * 10
+    const remainder = numberOfWheels - sumOfWheels
+    const sumOfBus = sumOfWheels / 10
+    const numberWithTwoAxles = remainder / 4
+    const numberWithThreeAxles = remainder / 6
+    var sumTwoAxles = 0
+    var sumThreeAxles = 0
     if(remainder % 4 === 0){
-        var sum = sumOfBus + numberWithTwoAxles
-        console.log("Liczba autobusów z 3 osiami: " + sumOfBus + ", Liczba autobusów z 2 osiami: " + sum )
+        var sumTwoAxles = sumOfBus + numberWithTwoAxles
+        return "Liczba autobusów z 3 osiami: " + sumOfBus + ", Liczba autobusów z 2 osiami: " + sumTwoAxles
     } else if (remainder % 6 === 0){
-        var sum = sumOfBus + numberWithThreeAxles
-        console.log("Liczba autobusów z 3 osiami: " + sumOfBus + ", Liczba autobusów z 2 osiami: " + sum)
+        var sumThreeAxles = sumOfBus + numberWithThreeAxles
+        return "Liczba autobusów z 3 osiami: " + sumOfBus + ", Liczba autobusów z 2 osiami: " + sumThreeAxles
     }
 }
 
