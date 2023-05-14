664c69a52107454a421cf5ad6dcc5e65f99c2716
Ania <aniaa.pawlik@gmail.com>
2023-05-15 00:21:40 +0200

    code prep

--- autobus/autobus.js
+++ autobus/autobus.js
@@ -1,10 +1,10 @@
 // https://codeforces.com/problemset/problem/1679/a 
 function autobus(numberOfWheels){
     if (numberOfWheels % 4 === 0){
-        return "Liczba autobusów z 2 osiami: " + numberOfWheels / 4
+        return "Number of buses with 2 axles: " + numberOfWheels / 4
     }
     if(numberOfWheels % 6 === 0) {
-        return "Liczba autobusów z 3 osiami: " + numberOfWheels / 6
+        return "Number of buses with 3 axles: " + numberOfWheels / 6
     }
     const sumOfWheels = Math.floor(numberOfWheels / 10) * 10
     const remainder = numberOfWheels - sumOfWheels
@@ -15,10 +15,10 @@ function autobus(numberOfWheels){
     var sumThreeAxles = 0
     if(remainder % 4 === 0){
         var sumTwoAxles = sumOfBus + numberWithTwoAxles
-        return "Liczba autobusów z 3 osiami: " + sumOfBus + ", Liczba autobusów z 2 osiami: " + sumTwoAxles
+        return "Number of buses with 3 axles: " + sumOfBus + ", Number of buses with 2 axles: " + sumTwoAxles
     } else if (remainder % 6 === 0){
         var sumThreeAxles = sumOfBus + numberWithThreeAxles
-        return "Liczba autobusów z 3 osiami: " + sumOfBus + ", Liczba autobusów z 2 osiami: " + sumThreeAxles
+        return "Number of buses with 3 axles: " + sumOfBus + ", Number of buses with 2 axles: " + sumThreeAxles
     }
 }
 
