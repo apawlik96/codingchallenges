65a8efdd566962a12a1b9ff0c2a42106512b3e9c
Ania <aniaa.pawlik@gmail.com>
2023-05-16 13:17:13 +0200

    code prep

--- twosumiiinputarrayissorted/two-sum-ii-input-array-is-sorted.js
+++ twosumiiinputarrayissorted/two-sum-ii-input-array-is-sorted.js
@@ -1,11 +1,11 @@
 // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 function twoSumIIInputArrayIsSorted (numbers, target) {
     var sum = 0;
-    const result = [];
-    for(let i=0;i <numbers.length; i++){
-        var sum = numbers[0] + numbers[i]
-        var firstValue = numbers.indexOf(numbers[0]) + 1
-        var secondValue = numbers.indexOf(numbers[i]) + 1
+    let result = [];
+    for(const numb of numbers){
+        var sum = numbers[0] + numb
+        let firstValue = numbers.indexOf(numbers[0]) + 1
+        let secondValue = numbers.indexOf(numb) + 1
         if(sum === target ){
             result.push(firstValue , secondValue) 
             return result
