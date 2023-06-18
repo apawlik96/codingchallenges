782b25d7378dc182aa3281af2cc29f6a40d0da8b
Ania <aniaa.pawlik@gmail.com>
2023-06-18 23:19:47 +0200

    task implementation

---
+++ longestconsecutivesequence/longest-consecutive-sequence.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
+function longestConsecutiveSequence(nums) {
+    nums.sort((a,b) => a-b)
+    let result = []
+    for (let i=0; i<=nums.length; i++){
+        if (nums[i+1] - nums[i] === 1) result.push(nums[i])
+    }
+    return result.length + 1
+}
+
+longestConsecutiveSequence([100,4,200,1,3,2])
