6caeb9abd878f1f16c7e03b503cc8a1a77c758ae
Ania <aniaa.pawlik@gmail.com>
2023-06-18 23:29:00 +0200

    task implementation

---
+++ oddevenlinkedlist/odd-even-linked-list.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/odd-even-linked-list/
+function oddEvenLinkedList(head){
+    let arrayWithOddIdx = []
+    let arrayWitheEvenIdx = []
+    for(let i=0; i<head.length; i++){
+        i % 2 !== 0 ? arrayWitheEvenIdx.push(head[i]) : arrayWithOddIdx.push(head[i])
+    }
+    return arrayWithOddIdx.concat(arrayWitheEvenIdx)
+}
+
+oddEvenLinkedList([2,1,3,5,6,4,7])
