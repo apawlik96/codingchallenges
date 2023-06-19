a1214b840b809dcd7c3aa8c42e2e43ec23508974
Ania <aniaa.pawlik@gmail.com>
2023-06-20 00:01:10 +0200

    code prep

--- largestnumber/largest-number.js
+++ largestnumber/largest-number.js
@@ -1,7 +1,15 @@
 // https://leetcode.com/problems/largest-number/
 function largestNumber (nums) {
     nums.sort().reverse()
-    return nums.join("")
+    let array = []
+    let lastIdx = []
+    nums.forEach(numb => {
+        numb % 10 !== 0 ? array.push(numb) : lastIdx.push(numb)
+    })
+    let result = array.concat(lastIdx)
+    return result.join("") 
 }
 
 largestNumber([10,2])
+
+module.exports = largestNumber
