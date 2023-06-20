b3414ba35d6ec766680e5380d9c58fc2581571bb
Ania <aniaa.pawlik@gmail.com>
2023-06-20 11:40:54 +0200

    code prep

--- containsduplicateiii/contains-duplicate-iii.js
+++ containsduplicateiii/contains-duplicate-iii.js
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/contains-duplicate-iii/
 function containsDuplicateIII(nums, indexDiff, valueDiff){
-    return indexDiff !== valueDiff && indexDiff - valueDiff <= valueDiff && nums[indexDiff] - nums[valueDiff] <= valueDiff ? true : false
+    let result = []
+    if (indexDiff !== valueDiff) result.push(1)
+    if (indexDiff - valueDiff <= valueDiff) result.push(1)
+    if (nums[indexDiff] - nums[valueDiff] <= valueDiff ) result.push(1)
+    return result.length ===3 ? true : false
 }
 
 containsDuplicateIII([1,2,3,1], 0, 3)
+
+module.exports = containsDuplicateIII
