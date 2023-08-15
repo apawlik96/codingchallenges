afbf0021924f910cb28f89489243186bffeec2f0
Ania <aniaa.pawlik@gmail.com>
2023-08-16 00:58:03 +0200

    code prep

--- rankscores/rank-scores.js
+++ rankscores/rank-scores.js
@@ -1,21 +1,21 @@
 // https://leetcode.com/problems/rank-scores/description/
-function rankScores(score, id){
-	score.sort().reverse()
-	const mySet = new Set(score)
+function rankScores(score, id) {
+    score.sort().reverse()
+    const mySet = new Set(score)
     const length = mySet.size
     id.splice(length)
     let result = []
-    for(let i=0; i<id.length; i++){
-        result.push(i+1)
+    for (let i = 0; i < id.length; i++) {
+        result.push(i + 1)
     }
-    for(let i=0; i<score.length; i++){
-        if(score[i] === score[i+1]) result.push(i)
+    for (let i = 0; i < score.length; i++) {
+        if (score[i] === score[i + 1]) result.push(i)
     }
     result.sort()
-    if(result[0] === 0 ) result.splice(0,1,1)
+    if (result[0] === 0) result.splice(0, 1, 1)
     return result
 }
 
-rankScores([3.50, 4.00, 3.65, 3.65, 3.85, 4.00], [1,2,3,4,5,6])
+rankScores([3.50, 4.00, 3.65, 3.65, 3.85, 4.00], [1, 2, 3, 4, 5, 6])
 
 module.exports = rankScores
